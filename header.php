012e3c244bfa211dc1d6c033e70dea4f72117952
Jesus Cuenca <jesus.cuenca@gmail.com>
2016-02-09 18:44:25 +0100

    Integrate header image

--- header.php
+++ header.php
@@ -26,6 +26,14 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
+
+		   <?php $header_image = get_header_image();
+		      if ( ! empty( $header_image ) ) { ?>
+		         <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+			    <img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" />
+			 </a>
+	           <?php } // if ( ! empty( $header_image ) )?>
+
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
